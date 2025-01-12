a1814ff815ba26747794157f0457ab5b3ed75941
Steven221-afk <placevoting@gmail.com>
2025-01-12 19:49:32 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,15 +7,14 @@ import { getAuth , createUserWithEmailAndPassword ,signInWithEmailAndPassword , 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBCRCVJh3xEJWlAfRWs84QAnx8oSPKOdd4",
-  authDomain: "ghulam-mustafa-50d0d.firebaseapp.com",
-  projectId: "ghulam-mustafa-50d0d",
-  storageBucket: "ghulam-mustafa-50d0d.appspot.com",
-  messagingSenderId: "862111048332",
-  appId: "1:862111048332:web:f34e989d8401842eb3d1a9",
-  measurementId: "G-CHNG627ZXD"
+  apiKey: "AIzaSyAsz1oCCnC6cfGuz-BgypNU5vfGyhNmORw",
+  authDomain: "tmx-27db6.firebaseapp.com",
+  projectId: "tmx-27db6",
+  storageBucket: "tmx-27db6.firebasestorage.app",
+  messagingSenderId: "1039087524135",
+  appId: "1:1039087524135:web:91792eb81974c186c43102",
+  measurementId: "G-88W0C2WRJB"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
